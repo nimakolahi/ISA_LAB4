98c3b1fdb76e4eec16be9c1a0b40a8ffe71cadcd
NIMA KOLAHIMAHMOUDI <87775939+nimakolahi@users.noreply.github.com>
2022-04-07 19:46:11 +0430

    Update packet_in.sv

--- lab4/tb/packet_in.sv
+++ lab4/tb/packet_in.sv
@@ -1,7 +1,7 @@
 class packet_in extends uvm_sequence_item;
     rand bit [31:0] A;
     rand bit [31:0] B;
-    constraint normalized {((A[22:0] * B[22:0]) && 32h'80000000) != 32'h80000000 -> A[30:23]+B[30:23] + 127> 0;}
+    constraint normalized {((A[22:0] * B[22:0]) && 32'h80000000) != 32'h80000000 -> A[30:23]+B[30:23] > 127;}
     `uvm_object_utils_begin(packet_in)
         `uvm_field_int(A, UVM_ALL_ON|UVM_HEX)
         `uvm_field_int(B, UVM_ALL_ON|UVM_HEX)
